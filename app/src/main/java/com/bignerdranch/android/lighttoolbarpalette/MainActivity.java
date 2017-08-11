ee9cc01b78bc0f64fa54b1c925b624563395c826
wanglianghai <1540446221@qq.com>
2017-08-11 21:31:45 +0800

    finish

--- app/src/main/java/com/bignerdranch/android/lighttoolbarpalette/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/lighttoolbarpalette/MainActivity.java
@@ -1,7 +1,13 @@
 package com.bignerdranch.android.lighttoolbarpalette;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,6 +15,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerTool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +28,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer);
+
         //在设置给activity的bar前写，之后没效果
         mToolbar.setTitle("Tool Bar");
         setSupportActionBar(mToolbar);
-        //是否给左上角图标的左边加上一个返回的图标
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //放在设置了之后，放前面，设置了后没效果，关联不起来
+        setLeftCornerClick();
+
+
         getSupportActionBar().setLogo(R.drawable.ic_action_name);
         mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
@@ -43,6 +55,29 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        //palette
+        palette();
+    }
+
+    private void palette() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.star);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                Palette.Swatch swatch = palette.getVibrantSwatch();
+                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(swatch.getRgb()));
+            }
+        });
+    }
+
+    private void setLeftCornerClick() {
+        //是否给左上角图标的左边加上一个返回的图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //放后面，放在放图标的后面，放前面没效果
+        mDrawerTool = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open, R.string.close);
+        mDrawerTool.syncState();
+        mDrawer.addDrawerListener(mDrawerTool);
     }
 
     @Override
