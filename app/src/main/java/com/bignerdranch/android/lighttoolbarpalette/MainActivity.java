a6ff5cf476a5e427ce9117b13d8ded27adceb1ee
wanglianghai <1540446221@qq.com>
2017-08-11 17:05:27 +0800

    tool bar监听菜单

--- app/src/main/java/com/bignerdranch/android/lighttoolbarpalette/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/lighttoolbarpalette/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -13,8 +15,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+    }
+
+    private void initView() {
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        //在设置给activity的bar前写，之后没效果
+        mToolbar.setTitle("Tool Bar");
         setSupportActionBar(mToolbar);
+        //是否给左上角图标的左边加上一个返回的图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_action_name);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.share_bar:
+                        Toast.makeText(MainActivity.this, "share bar", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.configuration:
+                        Toast.makeText(MainActivity.this, "configuration", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 
     @Override
